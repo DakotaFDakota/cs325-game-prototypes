8a7ef76445d0c9bc65eb943ec9979d5d03973cbf
Dakota <dakotaferris@gmail.com>
2021-02-06 15:12:58 -0500

    d

--- test-game/game.js
+++ test-game/game.js
@@ -99,12 +99,12 @@ function update() {
 
   player.body.velocity.x = 0;
 
-  if (cursors.left.isDown){
+  if (cursors.left.isDown(Phaser.Keyboard.LEFT)){
     player.body.velocity.x = -160;
 
     player.anims.play('left', true);
   }
-  else if (cursors.right.isDown) {
+  else if (cursors.right.isDown(Phaser.Keyboard.LEFT)) {
     player.body.velocity.x = 160;
 
     //player.anims.play('right', true)

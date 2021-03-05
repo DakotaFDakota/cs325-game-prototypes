b4b5f883d5bdded347d072cebbbb838c9fc26ac2
Dakota <dakotaferris@gmail.com>
2021-03-04 20:08:27 -0500

    a

--- game2v2/js/MainScene.js
+++ game2v2/js/MainScene.js
@@ -8,7 +8,6 @@ export default class MainScene extends Phaser.Scene{
         var carts;
         var cartscollect;
         this.numcarts = Math.floor(Math.random() * Math.floor(30));
-        //this.timer = function() {this.counter--};
     }
     
 
@@ -26,16 +25,10 @@ export default class MainScene extends Phaser.Scene{
     }
 
     create(){
-        //const cartmap = this.make.tilemap({key: 'cartmap'});
-        //const tileset = cartmap.addTilesetImage('tileset', 'street', 32, 32, 0, 0);
-        //const layer1 = cartmap.createStaticLayer('Tile Layer 1', tileset, 0, 0);
-        //this.matter.world.convertTilemapLayer(layer1);
         this.cartscollect = 0;
 
         this.add.image(0,0,'lotmap').setOrigin(0,0);
         this.carts = this.add.text(10,475, 'Carts Collected: 0');
-
-        //this.cart = new cart({scene: this, x: 80, y: 80, texture: 'cart', frame: 'cart'});
 
         for(let i = 0; i < this.numcarts; i++){
             let fakecart = this.add.image(Math.random() * Math.floor(400),Math.random() * Math.floor(400), 'cart');
@@ -45,12 +38,6 @@ export default class MainScene extends Phaser.Scene{
         }
 
 
-
-        //var testpic = this.load.image('cart');
-        //var testtext = this.add.text(Math.random() * Math.floor(400),Math.random() * Math.floor(400), 'cart');
-        //testtext.setInteractive();
-        //testtext.on('pointerdown', () => this.clicked(testtext));
-        
 
         this.counter = this.add.text(445, 20, this.count, {fill: this.fillcolor});
         var timer = this.time.addEvent({
@@ -66,24 +53,19 @@ export default class MainScene extends Phaser.Scene{
         this.count--;
         if(this.count === 0 && this.cartscollect != this.numcarts){
             this.add.text(150,150,"YOU LOSE", {font: '50px Times New Roman', fill: '#FF0000'});
-            //timer.remove();
             }
     }
     
 
     update(){
         console.log(this.numcarts);
-        //let pointer = this.scene.input.activePointer;
         this.carts.setText('Carts Collected: ' + this.cartscollect + ' / ' + this.numcarts);
         if (this.count <= 5) {
             this.fillcolor = '#FF0000';
         }
         else {
             this.fillcolor = '#FFFFFF'
-        }
-        //if(this.count === 0){
-          //  timer.remove();
-        //}        
+        }     
         if(this.cartscollect === this.numcarts && this.count > 0){
             this.add.text(150,150,"YOU WIN", {font: '50px Times New Roman', fill: '#FFFFFF'});
         }
